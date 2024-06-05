0b5b3c84cfcedd075ed0aea873eb6f362856b48a
tomotomoz <tomoko.essvivh@gmail.com>
2024-06-06 05:29:58 +0900

    gsap

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const btn = document.querySelector('.hamburger')
 btn.addEventListener('click', function() {
 nav.classList.toggle('open')
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+const contents = gsap.utils.toArray('.fade')
+console.log(contents)
+
+contents.forEach((content) => {
+  gsap.to(content, {
+  
+scrollTrigger: {
+  trigger: content,
+  start: 'top center',
+  // markers: true
+},
+  y: 0,
+  opacity: 1,
+  })
+  });
